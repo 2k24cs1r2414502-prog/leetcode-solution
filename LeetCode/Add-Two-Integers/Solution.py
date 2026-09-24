56ec6a22af5d2f860cbdf001847991d333cbd6e8
DIVY PANDEY <2k24.cs1r.2414502@gmail.com>
2026-09-24 12:33:54 +0530

    Add solution for Add Two Integers

---
+++ LeetCode/Add-Two-Integers/Solution.py
@@ -0,0 +1,9 @@
+1class Solution(object):
+2    def sum(self, num1, num2):
+3        """
+4        :type num1: int
+5        :type num2: int
+6        :rtype: int
+7        """
+8        sum1=num1+num2
+9        return sum1
